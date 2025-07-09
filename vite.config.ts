eecb88e50541f6c860531f5a81cc940b5d04947e
suresh <suresh.i@rjaytechnologies.com>
2025-07-09 11:05:40 +0530

    Update Vite base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/RG_infra_developers/', // ✅ THIS is the fix
+  base: '/', // 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
